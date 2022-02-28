f745e5720a96f0e432dcf6962f9dadbbae26a5cb
Le Huu Hung <huuhung1962001@gmail.com>
2022-02-28 15:50:14 +0700

    co y nghia

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ export default function App() {
       <Text> Hello Loc</Text>
 
       <Text> Hellu Hung</Text>
+      
+      <Text> this is HUng</Text>
+      <Text> Hellu Hung</Text>
+      <Text> Hellu Hung</Text>
 
       
 
