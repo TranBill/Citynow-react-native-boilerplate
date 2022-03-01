46a1444605ddc3fcb7c80abb04189133d7ae5669
Long Luu <luuhoanglong577@gmail.com>
2022-03-01 14:08:08 +0700

    use ESlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/react-in-jsx-scope */
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
       <Text> Hello Longluu</Text>
@@ -10,7 +12,7 @@ export default function App() {
       <Text> Hellu Hung</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
