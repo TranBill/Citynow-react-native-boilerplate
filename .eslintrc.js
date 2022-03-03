1022bc469a41cb12880559bb352ec84f4dd4789a
Long Luu <luuhoanglong577@gmail.com>
2022-03-03 09:53:25 +0700

    config ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,18 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "standard"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
   }
-}
+};
